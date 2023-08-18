51e1262b985726a9ddd16280be4a6caccb9fa714
Lokie89 <traeuman@gmail.com>
2023-08-18 15:25:10 +0900

    feat : issue field id
    - 이슈 Id 필드 추가
    - 이슈 동일성 Id 의 동일성으로 책임 전가

--- src/main/java/com/ilililissue/issue/domain/entity/Issue.java
+++ src/main/java/com/ilililissue/issue/domain/entity/Issue.java
@@ -1,17 +1,23 @@
 package com.ilililissue.issue.domain.entity;
 
 import com.ilililissue.issue.domain.UnauthorizedException;
+import com.ilililissue.issue.domain.vo.Id;
 import com.ilililissue.member.domain.Member;
+import lombok.EqualsAndHashCode;
 
 /**
  * @author SeongRok.Oh
  * @since 2023/08/18
  */
+@EqualsAndHashCode(of = "id")
 public class Issue {
-
+    private Id id;
     private Member register;
 
-    public Issue(Member register) {
+    public Issue(
+            Id id,
+            Member register) {
+        this.id = id;
         this.register = register;
         this.validate();
     }
--- src/test/java/com/ilililissue/issue/domain/entity/IssueTest.java
+++ src/test/java/com/ilililissue/issue/domain/entity/IssueTest.java
@@ -1,5 +1,6 @@
 package com.ilililissue.issue.domain.entity;
 
+import com.ilililissue.issue.domain.vo.Id;
 import com.ilililissue.member.domain.Member;
 import com.ilililissue.member.domain.MemberRole;
 import org.assertj.core.api.Assertions;
@@ -15,6 +16,31 @@ public class IssueTest {
     void createIssue() {
         Member member = new Member(MemberRole.GUEST);
 
-        Assertions.assertThatThrownBy(() -> new Issue(member));
+        Assertions.assertThatThrownBy(() -> new Issue(null, member));
+    }
+
+    @Test
+    void equals() {
+        Member originRegister = new Member(MemberRole.ADMINISTRATOR);
+        Id originId = new Id(5);
+        Issue originIssue = new Issue(originId, originRegister);
+
+        Issue sameIdAndRegisterIssue = new Issue(originId, originRegister);
+        Assertions.assertThat(originIssue).isEqualTo(sameIdAndRegisterIssue);
+
+        Id sameValueId = new Id(5);
+
+        Issue sameValueIdAndRegisterIssue = new Issue(sameValueId, originRegister);
+        Assertions.assertThat(originIssue).isEqualTo(sameValueIdAndRegisterIssue);
+
+        Member sameRoleRegister = new Member(MemberRole.ADMINISTRATOR);
+
+        Issue sameValueIdAndSameRoleRegisterIssue = new Issue(sameValueId, sameRoleRegister);
+        Assertions.assertThat(originIssue).isEqualTo(sameValueIdAndSameRoleRegisterIssue);
+
+        Id differentValueId = new Id(6);
+        Issue differentValueIdAndSameRegisterIssue = new Issue(differentValueId, originRegister);
+        Assertions.assertThat(originIssue).isNotEqualTo(differentValueIdAndSameRegisterIssue);
+
     }
 }
